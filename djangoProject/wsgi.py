235a557e0dad6643f1c3bf42d9644e75f2a9ce54
1akhmadjon <139978632+1akhmadjon@users.noreply.github.com>
2024-09-21 16:19:36 +0500

    Update wsgi.py

--- djangoProject/wsgi.py
+++ djangoProject/wsgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-import gunicorn
 from django.core.wsgi import get_wsgi_application
 
 import djangoProject
@@ -16,5 +15,3 @@ import djangoProject
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings')
 
 application = get_wsgi_application()
-
-web: gunicorn djangoProject.wsgi --log-file -
