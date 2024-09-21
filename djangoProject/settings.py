8f6e376cc450d1871bab06d2c68be67c9cc5b452
user <user@kali.user>
2024-09-21 15:25:22 +0500

    requirements

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -76,10 +76,22 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Mbek001$erixconsulting',
+        'USER': 'Mbek001',
+        'PASSWORD': 'erixconsulting',
+        'HOST': 'Mbek001.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
